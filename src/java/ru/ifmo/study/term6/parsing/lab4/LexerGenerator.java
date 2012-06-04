a5e6c74f786257c790d6401407ba857a8873c8a6
Niyaz Nigmatullin <niyaz@virtualbox-debian.ctd.ifmo.ru>
2012-06-04 18:13:30 +0400

    idea project created

---
+++ src/java/ru/ifmo/study/term6/parsing/lab4/LexerGenerator.java
@@ -0,0 +1,11 @@
+package ru.ifmo.study.term6.parsing.lab4;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: niyaz
+ * Date: 6/4/12
+ * Time: 6:12 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LexerGenerator {
+}
