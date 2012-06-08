43a107928eeebee0bb9da3bd670306f0e01126d8
Niyaz Nigmatullin <niyaz.nigmatullin@gmail.com>
2012-06-08 19:08:25 +0400

    starting to do a project

--- src/java/ru/ifmo/study/term6/parsing/lab4/ParserGenerator.java
+++ src/java/ru/ifmo/study/term6/parsing/lab4/ParserGenerator.java
@@ -3,10 +3,8 @@ package ru.ifmo.study.term6.parsing.lab4;
 /**
  * Created with IntelliJ IDEA.
  * User: niyaz
- * Date: 6/4/12
- * Time: 6:12 PM
+ * Date: 6/8/12
+ * Time: 7:06 PM
  */
-
-public class LexerGenerator {
-
+public class ParserGenerator {
 }
